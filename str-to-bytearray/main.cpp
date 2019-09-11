eb32dce32c65eaf57bcce2d5de035463edad630d
Joakim M <10110847+p--xy@users.noreply.github.com>
2019-09-11 22:09:52 +0200

    renamed and removed unnecessary windows header

--- str-to-bytearray/main.cpp
+++ str-to-bytearray/main.cpp
@@ -1,14 +1,12 @@
-#include "szdf.h"
-#define LEAN_AND_MEAN
-#include <Windows.h>
+#include "signature.h"
 
 int main() {
-    constexpr auto pattern = make_signature("DE AD ?? BE EF");
+    constexpr auto signature = make_signature("DE AD ?? BE EF");
 
     std::array<uint8_t, 16> x = {0xFF, 0xFF, 0xFF, 0xFF, 0x23, 0xDE, 0xAD, 0xFF,
                                  0xBE, 0xEF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
 
-    auto results = search_address(x.data(), x.data() + x.size(), pattern);
+    auto results = search_address(x.data(), x.data() + x.size(), signature);
 
     return reinterpret_cast<int>(results);
 }
